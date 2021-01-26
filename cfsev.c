dbfce742cdb00a2943f69fe5d92d31eaf57c72c1
Souza-Lucas <lucasvinicius0511@gmail.com>
2021-01-26 17:28:30 -0300

    versâo 0.2

--- cfsev.c
+++ cfsev.c
@@ -11,32 +11,226 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void menuSobre(void);
+/// 
+/// Assinaturas das funções
+///
+void menuPrincipal(void);
+void telaSobre(void);
+void menuUsuario(void);
+void menuVagas(void);
+void telaCadastrarUsuario(void);
+void menuHistorico(void);
+
+
+///
+/// Programa Principal
+///
 int main(void) {
-    menuSobre();
+    telaSobre();
+    menuPrincipal();
+    menuUsuario();
+    menuVagas();
+    telaCadastrarUsuario();
+    menuHistorico();
     return 0;
 }
 
 
-void menuSobre(void) {
-    printf(" __________________________________________________________________________________\n");
-    printf("|                                                                                  |\n");
-    printf("|                    Universidade Federal do Rio Grande do Norte                   |\n");
-    printf("|                        Centro de Ensino Superior do Serido                       |\n");
-    printf("|                         Disciplina DCT1106 -- Programação                        |\n");
-    printf("|                       Projeto Controle de fluxo de veiculos                      |\n");
-    printf("|__________________________________________________________________________________|\n");
-    printf("|                                                                                  |\n");
-    printf("|                              = = = = CFSEV = = = =                               |\n");
-    printf("|                           Controle do fluxo de veiculos                          |\n");
-    printf("|__________________________________________________________________________________|\n");
-    printf("|                                                                                  |\n");
-    printf("|          Programa utilizado na disciplina DCT1106 - Programação. Esse pro-       |\n");
-    printf("|        grama ira controlar o fluxo de veiculos para a entrada e saida em         |\n");
-    printf("|        estacionamentos, tendo como alvo as instituicoes publicas ou priva-       |\n");
-    printf("|        das que oferecem estacionamento gratuito.                                 |\n");
-    printf("|__________________________________________________________________________________|\n");
-    printf("|                                                                                  |\n");
-    printf("| Developed by Italo Guilherme (@italx_g) and Lucas Souza (@sza_lucas) - Jan, 2021 |\n");
-    printf("|__________________________________________________________________________________|\n");
+void menuPrincipal(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = =   Programa CFSEV    = = = = = = = =          ///\n");
+	printf("///          = Controle do Fluxo da Saida/Entrada de Veiculos  =          ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///           Developed by  @italx_g and @sza_lucas - Jan, 2021           ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = =  Menu Principal = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Modulo usuario                                           ///\n");
+	printf("///           2. Modulo vagas                                             ///\n");
+	printf("///           3. Modulo historico                                         ///\n");
+	printf("///           4. Informações sobre o sistema                              ///\n");
+	printf("///           0. Encerra o programa                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Escolha a opção desejada:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+void telaSobre(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///              Universidade Federal do Rio Grande do Norte              ///\n");
+	printf("///                  Centro de Ensino Superior do Seridó                  ///\n");
+	printf("///                Departamento de Computação e Tecnologia                ///\n");
+	printf("///                   Disciplina DCT1106 -- Programação                   ///\n");
+	printf("///        Projeto Controle do fludo da saida e entrada de veiculos       ///\n");
+	printf("///            Developed by  @italx_g and @sza_lucas - Jan, 2021          ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = =   Programa CFSEV    = = = = = = = =          ///\n");
+	printf("///          = Controle do Fluxo da Saida/Entrada de Veiculos  =          ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+    printf("///                                                                       ///\n");
+    printf("///  Programa utilizado na disciplina DCT1106 - Programação. Esse pro-    ///\n");
+    printf("///  grama ira controlar o fluxo de veiculos para a entrada e saida em    ///\n");
+    printf("///  estacionamentos, tendo como alvo as instituicoes publicas ou priva-  ///\n");
+    printf("///  das que oferecem estacionamento gratuito.                            ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+void menuUsuario(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = =   Programa CFSEV    = = = = = = = =          ///\n");
+	printf("///          = Controle do Fluxo da Saida/Entrada de Veiculos  =          ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///           Developed by  @italx_g and @sza_lucas - Jan, 2021           ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = =  Menu Usuario = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Cadastrar um novo Usuario                                ///\n");
+	printf("///           2. Pesquisar os dados de um Usuario                         ///\n");
+	printf("///           3. Atualizar o cadastro de um Usuario                       ///\n");
+	printf("///           4. Excluir um Usuario do sistema                            ///\n");
+	printf("///           0. Voltar ao menu anterior                                  ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Escolha a opção desejada:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+void menuVagas(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = =   Programa CFSEV    = = = = = = = =          ///\n");
+	printf("///          = Controle do Fluxo da Saida/Entrada de Veiculos  =          ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///           Developed by  @italx_g and @sza_lucas - Jan, 2021           ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = =  Menu Vagas = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Total de vagas                                           ///\n");
+	printf("///           2. Vagas livres                                             ///\n");
+	printf("///           3. Vagas em uso                                             ///\n");
+	printf("///           0. Voltar ao menu anterior                                  ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Escolha a opção desejada:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+void telaCadastrarUsuario(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = =   Programa CFSEV    = = = = = = = =          ///\n");
+	printf("///          = Controle do Fluxo da Saida/Entrada de Veiculos  =          ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///           Developed by  @italx_g and @sza_lucas - Jan, 2021           ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = Cadastrar Usuario = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Nome:                                                       ///\n");
+	printf("///           E-mail:                                                     ///\n");
+	printf("///           Celular:                                                    ///\n");
+	printf("///           Veiculo:                                                    ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+void menuHistorico(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = =   Programa CFSEV    = = = = = = = =          ///\n");
+	printf("///          = Controle do Fluxo da Saida/Entrada de Veiculos  =          ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///           Developed by  @italx_g and @sza_lucas - Jan, 2021           ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = Historico = = = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Historico do uso de vagas no ultimo dia                  ///\n");
+	printf("///           0. Voltar ao menu anterior                                  ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Escolha a opção desejada:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
 }

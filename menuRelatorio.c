dadc9b5d9a1c6f1e7f025af73faf6a6a3cefce1a
italoguilherme <77743713+italoguilherme@users.noreply.github.com>
2021-03-15 19:06:44 -0300

    Create menuRelatorio.c

---
+++ menuRelatorio.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void moduloRelatorio(void) {
+
+	menuRelatorio();
+
+}
+
+void menuRelatorio(void) {
+system("clear");
+		printf("\n");
+		printf("/////////////////////////////////////////////////////////////////////////////\n");
+		printf("///                                                                       ///\n");
+		printf("///              Universidade Federal do Rio Grande do Norte              ///\n");
+		printf("///                  Centro de Ensino Superior do Serido                  ///\n");
+		printf("///                Departamento de Computacao e Tecnologia                ///\n");
+		printf("///                   Disciplina DCT1106 -- Programacao                   ///\n");
+		printf("///        Projeto Controle do fludo da saida e entrada de veiculos       ///\n");
+		printf("///            Developed by  @italx_g and @sza_lucas - Jan, 2021          ///\n");
+		printf("///                                                                       ///\n");
+		printf("/////////////////////////////////////////////////////////////////////////////\n");
+		printf("///                                                                       ///\n");
+		printf("///          ===================================================          ///\n");
+		printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+		printf("///          = = = = = = = =   Menu Relatorio    = = = = = = = =          ///\n");
+		printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+		printf("///          ===================================================          ///\n");
+		printf("///                                                                       ///\n");
+		printf("///  . Armazenamento de informações                                       ///\n");
+		printf("///  . Melhorias no sistema de cadastro                                   ///\n");
+		printf("///                                                                       ///\n");
+		printf("///                                                                       ///\n");
+		printf("///                                                                       ///\n");
+		printf("/////////////////////////////////////////////////////////////////////////////\n");
+		printf("\n");
+		printf("\t\t\t>>> Tecle <ENTER> para voltar...\n");
+		getchar();
+}
